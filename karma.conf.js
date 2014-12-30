e7a873865a678eead94ce27dd82344e72976599c
georapbox <georapbox@gmail.com>
2014-12-30 10:26:49 +0200

    - let only PhantomJS do the job

--- karma.conf.js
+++ karma.conf.js
@@ -5,11 +5,11 @@ module.exports = function (config) {
 	config.set({
 		// base path that will be used to resolve all patterns (eg. files, exclude)
 		basePath: '',
-		
+
 		// frameworks to use
 		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
 		frameworks: ['jasmine'],
-		
+
 		plugins: [
     		'karma-htmlfile-reporter',
 			'karma-jasmine',
@@ -18,22 +18,22 @@ module.exports = function (config) {
 			'karma-chrome-launcher',
 			'karma-ie-launcher'
     	],
-		
+
 		// list of files / patterns to load in the browser
 		files: [
 			'src/*.js',
 			'tests/*.js'
 		],
-		
+
 		// list of files to exclude
 		exclude: [
 		],
-		
+
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
 		},
-		
+
 		// test results reporter to use
 		// possible values: 'dots', 'progress'
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
@@ -41,37 +41,37 @@ module.exports = function (config) {
 			'progress',
 			'html'
 		],
-		
+
 		htmlReporter: {
     		outputFile: 'tests/units.html'
     	},
-		
+
 		// web server port
 		port: 9876,
-		
+
 		// enable / disable colors in the output (reporters and logs)
 		colors: true,
-		
+
 		// level of logging
 		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
 		logLevel: config.LOG_INFO,
-		
+
 		// enable / disable watching file and executing tests whenever any file changes
 		autoWatch: true,
-		
+
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
 		browsers: [
-			'PhantomJS',
-			'Firefox',
-			'Chrome',
-            'IE7',
-            'IE8',
-            'IE9',
-            'IE10',
-            'IE'
+			'PhantomJS'
+			//'Firefox',
+			//'Chrome',
+            //'IE7',
+            //'IE8',
+            //'IE9',
+            //'IE10',
+            //'IE'
 		],
-		
+
 		customLaunchers: {
 			IE10: {
 				base: 'IE',
@@ -90,7 +90,7 @@ module.exports = function (config) {
 				'x-ua-compatible': 'IE=EmulateIE7'
 			}
 		},
-		
+
 		// Continuous Integration mode
 		// if true, Karma captures browsers, runs the tests and exits
 		singleRun: false
